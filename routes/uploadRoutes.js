beda543c007b245d58a3978fc0d7415eeff9f652
moula2002 <iammoulahussain@gmail.com>
2026-07-27 11:38:04 +0530

    new changes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,9 +4,15 @@ const multer = require('multer');
 const path = require('path');
 const { protect } = require('../middleware/authMiddleware');
 
+const fs = require('fs');
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'uploads/');
+    const dir = 'uploads/';
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    cb(null, dir);
   },
   filename(req, file, cb) {
     cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
